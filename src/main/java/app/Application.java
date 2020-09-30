4e780a77c7337fd715a10c7937325e8e821f48c3
ViktoriaGatsulia <viktoria.gatsulia@gmail.com>
2020-09-30 11:04:20 +0700

    Start spring project

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,17 @@
+package app;
+
+import org.apache.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    private static final Logger log = Logger.getLogger(Application.class.getName());
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        log.info("Start application!");
+    }
+
+}
